32df8293dd804070d03a6f965ce83f5efe81a652
Clay Ellis <clayellis@me.com>
2022-11-12 12:51:30 -0700

    Update CICD workflow

--- .github/workflows/cicd/CICD.swift
+++ .github/workflows/cicd/CICD.swift
@@ -3,8 +3,7 @@ import SwiftCI
 @main
 struct CICD: Workflow {
     func run() async throws {
-        print("HELLO...")
         try await step(.swiftBuild)
-        print("...WORLD!")
+        try await step(.swiftTest)
     }
 }
