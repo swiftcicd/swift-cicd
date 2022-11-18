e6a028ca33877381b6d700fcc11baf69fd7a9dbe
Clay Ellis <clayellis@me.com>
2022-11-17 17:31:38 -0700

    Update demo to load file from environment

--- Sources/Demo/DemoWorkflow.swift
+++ Sources/Demo/DemoWorkflow.swift
@@ -6,7 +6,7 @@ struct Demo: Workflow {
     static let logLevel: Logger.Level = .debug
 
     func run() async throws {
-        try await step(.swiftBuild)
-        try await step(.swiftTest)
+        let file = try await step(.loadFile(fromEnvironmentKey: "FILE", as: "file.txt")).loadedFile
+        print(file)
     }
 }
