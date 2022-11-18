de3bfba0931f78ff5d6741f7207a20846391a25e
Clay Ellis <clayellis@me.com>
2022-11-18 01:55:29 -0700

    Update demo workflow

--- Sources/Demo/DemoWorkflow.swift
+++ Sources/Demo/DemoWorkflow.swift
@@ -6,7 +6,7 @@ struct Demo: Workflow {
     static let logLevel: Logger.Level = .debug
 
     func run() async throws {
-        let file = try await step(.loadFile(fromEnvironmentKey: "FILE", as: "file.txt")).loadedFile
+        let file = try await step(.loadFile(fromEnvironmentKey: "FILE", as: "file.txt")).filePath
         print(file)
     }
 }
