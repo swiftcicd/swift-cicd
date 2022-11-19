f94bfb41047e7a3398bceb471631c9340e3d31f3
Clay Ellis <clayellis@me.com>
2022-11-19 16:31:22 -0700

    Secrets can specify source and encoding. Steps can load secrets as either string or data.

--- Sources/SwiftCI/Secret.swift
+++ Sources/SwiftCI/Secret.swift
@@ -1,4 +1,64 @@
-public enum Secret {
-    case environmentFile(String)
-    case environmentValue(String)
+import Foundation
+
+public struct Secret {
+    let source: Source
+    var encoding: Encoding?
+
+    public init(source: Source, encoding: Encoding?) {
+        self.source = source
+        self.encoding = encoding
+    }
+
+    public enum Source {
+        case environment(String)
+    }
+
+    public enum Encoding {
+        case base64
+    }
+}
+
+public protocol LoadedSecret {
+    init(secretString: String)
+    init(secretData: Data)
+}
+
+extension String: LoadedSecret {
+    public init(secretString: String) {
+        self = secretString
+    }
+
+    public init(secretData: Data) {
+        self.init(decoding: secretData, as: UTF8.self)
+    }
+}
+
+extension Data: LoadedSecret {
+    public init(secretString: String) {
+        self.init(secretString.utf8)
+    }
+
+    public init(secretData: Data) {
+        self = secretData
+    }
+}
+
+public extension Step {
+    func loadSecret<Loaded: LoadedSecret>(_ secret: Secret) throws -> Loaded {
+        switch secret.source {
+        case .environment(let key):
+            let value = try context.environment.require(key)
+            switch secret.encoding {
+            case .none:
+                return Loaded(secretString: value)
+
+            case .some(.base64):
+                guard let data = Data(base64Encoded: value, options: .ignoreUnknownCharacters) else {
+                    throw StepError("Failed to base64-decode secret")
+                }
+
+                return Loaded(secretData: data)
+            }
+        }
+    }
 }
