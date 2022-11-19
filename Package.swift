24ecfcd855592be7da92930b330cbbcd1e0b0e9b
Clay Ellis <clayellis@me.com>
2022-11-19 15:07:21 -0700

    Add JWTKit dependency

--- Package.swift
+++ Package.swift
@@ -12,17 +12,19 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/clayellis/swift-arguments", branch: "main"),
         .package(url: "https://github.com/clayellis/swift-environment", branch: "main"),
+        .package(url: "https://github.com/vapor/jwt-kit", from: "4.0.0"),
+        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.4"),
-        .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0")
     ],
     targets: [
         .target(
             name: "SwiftCI",
             dependencies: [
                 .product(name: "Arguments", package: "swift-arguments"),
+                .product(name: "JWTKit", package: "jwt-kit"),
+                .product(name: "Logging", package: "swift-log"),
                 .product(name: "ShellOut", package: "ShellOut"),
                 .product(name: "SwiftEnvironment", package: "swift-environment"),
-                .product(name: "Logging", package: "swift-log"),
             ]
         ),
         .testTarget(
