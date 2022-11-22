ca75594871aa1a50e1d4948cf7be62e3c6c7bb1b
Clay Ellis <clayellis@me.com>
2022-11-22 12:12:14 -0700

    Add first-party support for SwiftPR

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "swift-ci",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v12)],
     products: [
         .library(name: "SwiftCI", targets: ["SwiftCI"]),
         .executable(name: "Demo", targets: ["Demo"])
@@ -12,6 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/clayellis/swift-arguments", branch: "main"),
         .package(url: "https://github.com/clayellis/swift-environment", branch: "main"),
+        .package(url: "https://github.com/clayellis/swift-pr", branch: "main"),
         .package(url: "https://github.com/vapor/jwt-kit", from: "4.0.0"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.3.0"),
         .package(url: "https://github.com/apple/swift-log", from: "1.4.4"),
@@ -25,6 +26,7 @@ let package = Package(
                 .product(name: "Logging", package: "swift-log"),
                 .product(name: "ShellOut", package: "ShellOut"),
                 .product(name: "SwiftEnvironment", package: "swift-environment"),
+                .product(name: "SwiftPR", package: "swift-pr"),
             ]
         ),
         .testTarget(
