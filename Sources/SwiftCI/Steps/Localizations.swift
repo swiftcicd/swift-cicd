0b8c81e0215888cafdc47503c747cb7817b40fc7
Clay Ellis <clayellis@me.com>
2022-11-21 23:51:49 -0700

    Move localizations steps away from xcodebuild step

---
+++ Sources/SwiftCI/Steps/Localizations.swift
@@ -0,0 +1,54 @@
+public struct ImportLocalizations: Step {
+    var xcodeProject: String?
+    let localizationPath: String
+
+    public init(xcodeProject: String? = nil, localizationPath: String) {
+        self.xcodeProject = xcodeProject
+        self.localizationPath = localizationPath
+    }
+
+    public func run() async throws -> String {
+        var xcodebuild = Command("xcodebuild", "-importLocalizations", "-localizationPath", localizationPath)
+        xcodebuild.add("-project", ifLet: xcodeProject ?? context.xcodeProject)
+        return try context.shell(xcodebuild)
+    }
+}
+
+public struct ExportLocalizations: Step {
+    // TODO: Investigate using this argument:
+    // -exportLanguage (specifies multiple optional ISO 639-1 languages included in a localization export)
+    // We may be able to detect support languages from the project file and then pass those supported languages
+
+    var xcodeProject: String?
+    let localizationPath: String
+
+    public init(xcodeProject: String? = nil, localizationPath: String) {
+        self.xcodeProject = xcodeProject
+        self.localizationPath = localizationPath
+    }
+
+    public func run() async throws -> String {
+        var xcodebuild = Command("xcodebuild", "-exportLocalizations", "-localizationPath", localizationPath)
+        xcodebuild.add("-project", ifLet: xcodeProject ?? context.xcodeProject)
+        return try context.shell(xcodebuild)
+    }
+}
+
+public extension StepRunner {
+    func importLocalizations(fromDirectory localizationsDirectory: String, xcodeProject: String? = nil) async throws {
+        for file in try context.fileManager.contentsOfDirectory(atPath: localizationsDirectory) {
+            guard file.hasSuffix(".xcloc") else { continue }
+            try await step(ImportLocalizations(xcodeProject: xcodeProject, localizationPath: localizationsDirectory/file))
+        }
+    }
+
+    @discardableResult
+    func importLocalizations(from localizationPath: String, xcodeProject: String? = nil) async throws -> String {
+        try await step(ImportLocalizations(xcodeProject: xcodeProject, localizationPath: localizationPath))
+    }
+
+    @discardableResult
+    func exportLocalizations(to localizationPath: String, xcodeProject: String? = nil) async throws -> String {
+        try await step(ExportLocalizations(xcodeProject: xcodeProject, localizationPath: localizationPath))
+    }
+}
