b551903e1435198dd4e4d8334163d765d6916102
Clay Ellis <clayellis@me.com>
2022-11-26 21:27:16 -0700

    Allow attributes to be passed to SaveFile

--- Sources/SwiftCI/Steps/SaveFile.swift
+++ Sources/SwiftCI/Steps/SaveFile.swift
@@ -5,16 +5,18 @@ public struct SaveFile: Step {
 
     let fileName: String
     let contents: Data
+    var attributes: [FileAttributeKey: Any]?
     var parentDirectoryPath: String?
 
-    public init(name: String, contents: Data, parentDirectoryPath: String? = nil) {
+    public init(name: String, contents: Data, attributes: [FileAttributeKey: Any]?, parentDirectoryPath: String? = nil) {
         self.fileName = name
         self.contents = contents
+        self.attributes = attributes
         self.parentDirectoryPath = parentDirectoryPath
     }
 
-    public init(name: String, contents: String, parentDirectoryPath: String? = nil) {
-        self.init(name: name, contents: Data(contents.utf8), parentDirectoryPath: parentDirectoryPath)
+    public init(name: String, contents: String, attributes: [FileAttributeKey: Any]? = nil, parentDirectoryPath: String? = nil) {
+        self.init(name: name, contents: Data(contents.utf8), attributes: attributes, parentDirectoryPath: parentDirectoryPath)
     }
 
     public struct Output {
@@ -24,7 +26,7 @@ public struct SaveFile: Step {
     public func run() async throws -> Output {
         let filePath = (parentDirectoryPath ?? context.temporaryDirectory)/fileName
         self.savedFilePath = filePath
-        guard context.fileManager.createFile(atPath: filePath, contents: contents) else {
+        guard context.fileManager.createFile(atPath: filePath, contents: contents, attributes: attributes) else {
             throw StepError("Failed to save temporary file \(filePath)")
         }
         return Output(filePath: filePath)
@@ -38,11 +40,11 @@ public struct SaveFile: Step {
 }
 
 public extension StepRunner {
-    func saveFile(name: String, contents: Data, into parentDirectoryPath: String? = nil) async throws -> SaveFile.Output {
-        try await step(SaveFile(name: name, contents: contents, parentDirectoryPath: parentDirectoryPath))
+    func saveFile(name: String, contents: Data, attributes: [FileAttributeKey: Any]? = nil, into parentDirectoryPath: String? = nil) async throws -> SaveFile.Output {
+        try await step(SaveFile(name: name, contents: contents, attributes: attributes, parentDirectoryPath: parentDirectoryPath))
     }
 
-    func saveFile(name: String, contents: String, into parentDirectoryPath: String? = nil) async throws -> SaveFile.Output {
-        try await step(SaveFile(name: name, contents: contents, parentDirectoryPath: parentDirectoryPath))
+    func saveFile(name: String, contents: String, attributes: [FileAttributeKey: Any]? = nil, into parentDirectoryPath: String? = nil) async throws -> SaveFile.Output {
+        try await step(SaveFile(name: name, contents: contents, attributes: attributes, parentDirectoryPath: parentDirectoryPath))
     }
 }
