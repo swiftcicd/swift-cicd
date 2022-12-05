a5802126ac5ba5b62a631292284901716fc6cb5c
Clay Ellis <clayellis@me.com>
2022-12-05 16:55:55 -0700

    Add retry to getting swift pr comment

--- Sources/SwiftCI/Steps/RunSwiftPR.swift
+++ Sources/SwiftCI/Steps/RunSwiftPR.swift
@@ -1,3 +1,4 @@
+import Foundation
 import OctoKit
 import SwiftPR
 
@@ -16,10 +17,16 @@ public struct RunSwiftPR: Step {
     public func run() async throws -> Output {
         try await prCheck.main()
         let status = prCheck.statusState
-        guard let comment = try await prCheck.getSwiftPRComment() else {
-            throw StepError("SwiftPR comment not found")
+        // FIXME: If the swift-pr comment is created for the first time it may be slow to appear on the API.
+        // We may need to introduce a slight delay here, or potentially a retry.
+        // Try 3 times with one, two, three seconds in between before failing?
+        return try await retry(atIntervals: [0, 1, 2, 3]) {
+            guard let comment = try await prCheck.getSwiftPRComment() else {
+                throw StepError("SwiftPR comment not found")
+            }
+
+            return Output(status: status, comment: comment)
         }
-        return Output(status: status, comment: comment)
     }
 }
 
@@ -28,4 +35,33 @@ public extension StepRunner {
     func runSwiftPR(_ prCheck: PRCheck.Type) async throws -> RunSwiftPR.Output {
         try await step(RunSwiftPR(prCheck: prCheck))
     }
+}
+
+enum RetryError: Error {
+    case retryFailedAfterAllAttempts
+}
+
+func retry<R>(atIntervals backoff: [Double], operation: () async throws -> R) async throws -> R {
+    var backoff = backoff
+    while !backoff.isEmpty {
+        let delay = backoff.removeFirst()
+        if delay > 0 {
+            ContextValues.shared.logger.debug("Retrying in \(delay)...")
+            try await Task.sleep(nanoseconds: NSEC_PER_SEC * UInt64(delay))
+        }
+
+        do {
+            let result = try await operation()
+            return result
+        } catch {
+            ContextValues.shared.logger.debug("Attempt failed: \(error)")
+
+            if backoff.isEmpty {
+                ContextValues.shared.logger.debug("All attempts failed. Not retrying.")
+                throw error
+            }
+        }
+    }
+
+    throw RetryError.retryFailedAfterAllAttempts
 }
