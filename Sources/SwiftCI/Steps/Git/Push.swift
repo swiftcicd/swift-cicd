bfc7e1ec4e7426ad5ce991ca744464009be50864
Clay Ellis <clayellis@me.com>
2022-12-05 13:14:09 -0700

    Fix warning

--- Sources/SwiftCI/Steps/Git/Push.swift
+++ Sources/SwiftCI/Steps/Git/Push.swift
@@ -2,7 +2,7 @@ public struct Push: Step {
     public let name = "git push"
 
     public func run() async throws -> String {
-        var push = Command("git", "push")
+        let push = Command("git", "push")
         return try context.shell(push)
     }
 }
