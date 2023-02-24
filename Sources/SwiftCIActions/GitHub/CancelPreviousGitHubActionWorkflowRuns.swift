51b4f784b5311f725d64e08a1605d7588918d70d
Clay Ellis <clayellis@me.com>
2023-02-24 15:42:44 -0700

    Make cancel GitHub action runs action more flexible

--- Sources/SwiftCIActions/GitHub/CancelPreviousGitHubActionWorkflowRuns.swift
+++ Sources/SwiftCIActions/GitHub/CancelPreviousGitHubActionWorkflowRuns.swift
@@ -1,13 +1,16 @@
-import SwiftCICore
 import OctoKit
+import SwiftCICore
+import SwiftCIPlatforms
 
-public struct CancelExistingGitHubActionWorkflowRunsForCurrentPullRequest: Action {
+public struct CancelGitHubActionWorkflowRuns: Action {
+    let predicate: (Run) -> Bool
+
+    init(where predicate: @escaping (Run) -> Bool) {
+        self.predicate = predicate
+    }
+
     public func run() async throws {
-        let pullReuqestNumber = try context.environment.github.requirePullRequestNumber()
-        let runsToCancel = try await getCurrentWorkflowRuns {
-            $0.pullRequests?.contains(where: { $0.number == pullReuqestNumber }) ?? false
-                && $0.status == .queued || $0.status == .inProgress
-        }
+        let runsToCancel = try await getCurrentWorkflowRuns(where: predicate)
         for run in runsToCancel {
             do {
                 logger.info("Cancelling existing workflow run \(run.id)")
@@ -20,7 +23,16 @@ public struct CancelExistingGitHubActionWorkflowRunsForCurrentPullRequest: Actio
 }
 
 public extension Action {
-    func cancelExistingGitHubActionWorkflowRunsForCurrentPullRequest() async throws {
-        try await action(CancelExistingGitHubActionWorkflowRunsForCurrentPullRequest())
+    func cancelGitHubActionWorkflowRuns(where predicate: @escaping (Run) -> Bool = { _ in true }) async throws {
+        try await action(CancelGitHubActionWorkflowRuns(where: predicate))
+    }
+
+    func cancelExistingGitHubActionWorkflowRunsForCurrentPullRequest(where predicate: @escaping (Run) -> Bool = { _ in true }) async throws {
+        let pullReuqestNumber = try context.environment.github.requirePullRequestNumber()
+        try await cancelGitHubActionWorkflowRuns { run in
+            run.pullRequests?.contains(where: { $0.number == pullReuqestNumber }) ?? false
+                && run.status == .queued || run.status == .inProgress
+                && predicate(run)
+        }
     }
 }
