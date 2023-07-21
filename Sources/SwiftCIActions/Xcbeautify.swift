b6d06d8a63cbbc870e535c05f57e4d0d2cc9f030
Clay Ellis <clayellis@me.com>
2023-07-21 12:51:41 -0600

    Make Xcbeautify a tool.

--- Sources/SwiftCIActions/Xcbeautify.swift
+++ Sources/SwiftCIActions/Xcbeautify.swift
@@ -1,6 +1,6 @@
 import SwiftCICore
 
-public struct Xcbeautify: Action {
+public struct Xcbeautify: Tool {
     public struct Options {
         /// Only print tasks that have warnings or errors.
         public var quiet: Bool
@@ -40,25 +40,27 @@ public struct Xcbeautify: Action {
         }
     }
 
-    private static var binPath: String?
-    @State var xcbeautifyDirectory: String?
-
-    let command: ShellCommand
-    let options: Options
-
-    public init(command: ShellCommand, options: Options = .init()) {
-        self.command = command
-        self.options = options
+    public static var isInstalled: Bool {
+        get async {
+            do {
+                let output = try context.shell("which xcbeautify", quiet: true)
+                return !output.contains("not found")
+            } catch {
+                return false
+            }
+        }
     }
 
-    public func run() async throws -> String {
-        if try !isInstalled() {
-            try await install()
-        }
+    public static func install() async throws {
+        try context.shell("brew install xcbeautify")
+    }
 
-        let binPath = Self.binPath ?? "xcbeautify"
+    public static func uninstall() async throws {
+        try context.shell("brew uninstall xcbeautify")
+    }
 
-        var xcbeautify = ShellCommand("set -o pipefail && \(command) | \(binPath)")
+    public static func beautify(_ command: ShellCommand, options: Options = .init()) async throws -> String {
+        var xcbeautify = ShellCommand("set -o pipefail && \(command) | xcbeautify")
         xcbeautify.append("--quiet", if: options.quiet)
         xcbeautify.append("--quieter", if: options.quieter)
         xcbeautify.append("--preserve-unbeautified", if: options.preserveUnbeautified)
@@ -69,61 +71,12 @@ public struct Xcbeautify: Action {
         xcbeautify.append("--junit-report-filename", ifLet: options.jUnitReportFileName)
         return try context.shell(xcbeautify)
     }
+}
 
-    public func cleanUp(error: Error?) async {
-        do {
-            try uninstall()
-        } catch {
-            logger.error("Failed to uninstall xcbeautify")
-        }
-    }
-
-    func isInstalled() throws -> Bool {
-        do {
-            let output = try context.shell("which xcbeautify", quiet: true)
-            return !output.contains("not found")
-        } catch {
-            // 'which' will exit with status 1 (throw an error) when the tool isn't found.
-            // Prefer the installed version over the temporary bin path.
-            // But if the binPath has been cached from a previous build, treat it as installed.
-            return Self.binPath != nil
-        }
-    }
-
-    func install() async throws {
-        let currentDirectory = context.fileManager.currentDirectoryPath
-        defer {
-            do {
-                try context.fileManager.changeCurrentDirectory(currentDirectory)
-            } catch {
-                logger.error("Failed to return to current directory '\(currentDirectory)' after installing xcbeautify")
-            }
-        }
-
-        let temp = context.fileManager.temporaryDirectory.path
-        try context.fileManager.changeCurrentDirectory(temp)
-        let xcbeautify = temp/"xcbeautify"
-
-        if context.fileManager.fileExists(atPath: xcbeautify) {
-            try context.fileManager.removeItem(atPath: xcbeautify)
-        }
-
-        try context.shell("git clone https://github.com/tuist/xcbeautify.git")
-//        try context.shell("git clone --branch export-localizations-warnings-support --single-branch https://github.com/clayellis/xcbeautify.git")
-        xcbeautifyDirectory = xcbeautify
-
-        // 'make install' needs sudo permissions to copy into /usr/local/bin/
-        // So instead of running install, we'll build the xcbeautify and then cache its bin path
-        try context.fileManager.changeCurrentDirectory(xcbeautify)
-        let flags = "--configuration release --disable-sandbox"
-        try context.shell("swift build \(unescaped: flags)")
-        let binPath = try context.shell("swift build --show-bin-path \(unescaped: flags)")
-        Self.binPath = binPath/"xcbeautify"
-    }
-
-    func uninstall() throws {
-        if let xcbeautifyDirectory {
-            try context.fileManager.removeItem(atPath: xcbeautifyDirectory)
+public extension Tools {
+    var xcbeautify: Xcbeautify.Type {
+        get async throws {
+            try await self[Xcbeautify.self]
         }
     }
 }
@@ -131,6 +84,7 @@ public struct Xcbeautify: Action {
 public extension Action {
     @discardableResult
     func xcbeautify(_ command: ShellCommand, options: Xcbeautify.Options = .init()) async throws -> String {
-        try await action(Xcbeautify(command: command, options: options))
+        let xcbeautify = try await context.tools.xcbeautify
+        return try await xcbeautify.beautify(command, options: options)
     }
 }
