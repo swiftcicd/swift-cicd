3a961bd94bd99c1692c75b81277853ae2ede6cd0
Clay Ellis <clayellis@me.com>
2023-07-24 15:16:48 -0600

    Fix misspelling in comment

--- Sources/SwiftCICDCore/MainAction.swift
+++ Sources/SwiftCICDCore/MainAction.swift
@@ -5,7 +5,7 @@ public protocol MainAction: Action<Void> {
     static var logLevel: Logger.Level { get }
     init()
 
-    /// Runs before every action run by this action. Any errors thrown will stop the exection of the entire workflow.
+    /// Runs before every action run by this action. Any errors thrown will stop the execution of the entire workflow.
     func before() async throws
 }
 
