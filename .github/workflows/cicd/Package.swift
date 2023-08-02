185c18fccad41b0541721aeff73d883e4086aa26
Clay Ellis <clayellis@me.com>
2023-08-02 00:51:52 -0600

    Update package manifest

--- .github/workflows/cicd/Package.swift
+++ .github/workflows/cicd/Package.swift
@@ -1,17 +1,17 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "cicd",
-    platforms: [.macOS(.v12)],
+    platforms: [.macOS(.v13)],
     products: [
         .executable(name: "cicd", targets: ["cicd"])
     ],
     dependencies: [
-        .package(url: "https://github.com/clayellis/swift-ci", branch: "action")
+        .package(url: "https://github.com/swiftcicd/swift-cicd", branch: "main")
     ],
     targets: [
-        .executableTarget(name: "cicd", dependencies: [.product(name: "SwiftCI", package: "swift-ci")], path: ".")
+        .executableTarget(name: "cicd", dependencies: [.product(name: "SwiftCICD", package: "swift-cicd")], path: ".")
     ]
 )
