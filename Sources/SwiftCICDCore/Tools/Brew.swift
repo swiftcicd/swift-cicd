d78598ec10b6992f01338325bcf6c26289228206
Clay Ellis <clayellis@me.com>
2023-09-02 20:42:07 -0600

    Add todo

--- Sources/SwiftCICDCore/Tools/Brew.swift
+++ Sources/SwiftCICDCore/Tools/Brew.swift
@@ -4,6 +4,12 @@ public struct Brew: Tool {
     public static func install() async throws {
         // FIXME: This will fail because the install script requires sudo or a password.
         try await context.shell(#"/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)""#)
+        // TODO: Run the "Next Steps" commands
+        // We can either parse the output of install script and look for that section and the exact commands to run
+        // or we can run whoami and get the name of the user and then use that username and hardcode the follow Next Step commands.
+        // :
+        // (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/clayellis/.zprofile
+        // eval "$(/opt/homebrew/bin/brew shellenv)"
     }
 
     public static func uninstall() async throws {
