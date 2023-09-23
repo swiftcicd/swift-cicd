ce56d77bb9497ef8605e00471aa83b0da60db607
Clay Ellis <clayellis@me.com>
2023-09-23 13:00:53 -0600

    Allow actions builder to throw

--- Sources/SwiftCICDCore/ActionBuilder/Actions/Actions.swift
+++ Sources/SwiftCICDCore/ActionBuilder/Actions/Actions.swift
@@ -6,12 +6,22 @@ public struct Actions<A: Action>: _BuilderAction {
         self.inner = actions()
     }
 
+    public init(@ActionBuilder _ actions: () throws -> A) throws {
+        self.inner = try actions()
+    }
+
     public func run() async throws {
         try await run(inner)
     }
 }
 
 public extension Action {
+    /// An entry point to run a list of actions using a `ActionBuilder` from another action's `run` method.
+    /// This can be thought of as an "escape hatch" from imperative actions to declarative actions.
+    func actions<A: Action>(@ActionBuilder _ actions: @escaping () throws -> A) async throws {
+        try await self.run(Actions(actions))
+    }
+
     /// An entry point to run a list of actions using a `ActionBuilder` from another action's `run` method.
     /// This can be thought of as an "escape hatch" from imperative actions to declarative actions.
     func actions<A: Action>(@ActionBuilder _ actions: @escaping () -> A) async throws {
